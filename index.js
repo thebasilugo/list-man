ff4978159fcd1cfcc1652f3f68ed9ebaf315a550
Basil, Ugo <115357283+thebasilugo@users.noreply.github.com>
2023-05-08 21:04:46 +0000

    Update index.js
    addition of lists, error handling and saving of information to the browser

--- index.js
+++ index.js
@@ -1,14 +1,58 @@
-let signup = document.querySelector(".signup");
-let login = document.querySelector(".login");
-let slider = document.querySelector(".slider");
-let formSection = document.querySelector(".form-section");
+const inputBox = document.querySelector("#input-box");
+const listContainer = document.querySelector("#list-container");
+const renderError = document.querySelector("#error");
 
-signup.addEventListener("click", () => {
-	slider.classList.add("moveslider");
-	formSection.classList.add("form-section-move");
-});
+// Functions
+function errorMsg(){
+    renderError.textContent = "Error. You have to write something!";
+}
 
-login.addEventListener("click", () => {
-	slider.classList.remove("moveslider");
-	formSection.classList.remove("form-section-move");
-});
+function endTimeout(){
+    renderError.textContent = "";
+}
+
+function addTask(){
+    if(inputBox.value === ''){
+        errorMsg();
+        // Make the error message end/disappear.
+        setTimeout(endTimeout, 1000);
+        saveData();
+
+    }else {
+        let li = document.createElement("li");
+        li.textContent = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.textContent = "\u00d7";
+        li.appendChild(span);
+        saveData();
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e){
+    if (e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }else if (e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false)
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
+
+function thereExists(){
+    // If the To-Do exists?
+    if (li.includes(inputBox.value)){
+        alert("yes")
+    }else alert("no") 
+}
