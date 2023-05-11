006de5c61bbb7aeb92545f6864cd92e55a031307
Basil, Ugo <115357283+thebasilugo@users.noreply.github.com>
2023-05-11 21:52:20 +0000

    to-dos are now added on the click of the enter button

--- index.js
+++ index.js
@@ -2,57 +2,77 @@ const inputBox = document.querySelector("#input-box");
 const listContainer = document.querySelector("#list-container");
 const renderError = document.querySelector("#error");
 
+
+
+
 // Functions
-function errorMsg(){
-    renderError.textContent = "Error. You have to write something!";
-}
+    // Display an error message when the 'Add' button is clicked, without any text being entered
+    function errorMsg(){
+        renderError.textContent = "Error. You have to write something!";
+    }
 
-function endTimeout(){
-    renderError.textContent = "";
-}
+    // Remove the error message
+    function endTimeout(){
+        renderError.textContent = "";
+    }
 
-function addTask(){
-    if(inputBox.value === ''){
-        errorMsg();
-        // Make the error message end/disappear.
-        setTimeout(endTimeout, 1000);
-        saveData();
+    // Add a To-Do when the 'Add' button is pressed
+    function addTask(){
+        
+        if(inputBox.value === ''){
+            // If there's nothing in the textbox, then display the error message
+            errorMsg();
+            // Make the error message end/disappear.
+            setTimeout(endTimeout, 1000);
+            saveData();
 
-    }else {
-        let li = document.createElement("li");
-        li.textContent = inputBox.value;
-        listContainer.appendChild(li);
-        let span = document.createElement("span");
-        span.textContent = "\u00d7";
-        li.appendChild(span);
+            
+        }else {
+            // run 'listExists()'
+
+            // If there's text inside the textbox, then add the To-Do to the To-Do List
+            let li = document.createElement("li");
+            li.textContent = inputBox.value;
+            listContainer.appendChild(li);
+            let span = document.createElement("span");
+            span.textContent = "\u00d7";
+            li.appendChild(span);
+            saveData();
+        }
+        inputBox.value = "";
         saveData();
     }
-    inputBox.value = "";
-    saveData();
-}
 
-listContainer.addEventListener("click", function(e){
-    if (e.target.tagName === "LI"){
-        e.target.classList.toggle("checked");
-        saveData();
-    }else if (e.target.tagName === "SPAN"){
-        e.target.parentElement.remove();
-        saveData();
+    // Add a To-Do when the 'Enter' is pressed
+    inputBox.addEventListener("keypress", function(event) {
+        // If the user presses the "Enter" key
+        if (event.key === "Enter") {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the add-btn 'button' element with a click
+        document.querySelector(".add-btn").click();
+        }
+    }); 
+    
+    // Creating a list item, to save the typed information in the HTML
+    listContainer.addEventListener("click", function(e){
+        if (e.target.tagName === "LI"){
+            e.target.classList.toggle("checked");
+            saveData();
+        }else if (e.target.tagName === "SPAN"){
+            e.target.parentElement.remove();
+            saveData();
+        }
+    }, false)
+
+    // Saving the data to the browser's local storage so that reloads, refreshing or closing of the page doesn't affect it.
+    // NB: Clearing of browser cache relative to time that the To-Dos were saved will affect saved information
+    function saveData(){
+        localStorage.setItem("savedata", listContainer.innerHTML);
     }
-}, false)
 
-function saveData(){
-    localStorage.setItem("data", listContainer.innerHTML);
-}
-
-function showTask(){
-    listContainer.innerHTML = localStorage.getItem("data");
-}
-showTask();
-
-function thereExists(){
-    // If the To-Do exists?
-    if (li.includes(inputBox.value)){
-        alert("yes")
-    }else alert("no") 
-}
+    // Display of saved data to localstorage upon restoration of webpage
+    function showTask(){
+        listContainer.innerHTML = localStorage.getItem("savedata");
+    }
+    showTask();
