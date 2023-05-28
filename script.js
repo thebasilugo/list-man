0d68107f3673a4e59ce015998922e00f8ba9a761
Basil, Ugo <115357283+thebasilugo@users.noreply.github.com>
2023-05-28 17:52:27 +0000

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ window.addEventListener('load', () => {
     function endTimeout(){
         renderError.textContent = "";
         renderHint.textContent = "";
-    }
+    }   
 
     // Add a To-Do when the 'Add' button is pressed
     function addTask(){
@@ -43,17 +43,22 @@ window.addEventListener('load', () => {
             saveData();
    
         }else {
-            // run 'listExists()'
-
             // If there's text inside the textbox, then add the To-Do to the To-Do List
-            let li = document.createElement("li");
+            const li = document.createElement("li");
+            // let a = document.createElement("a");
+            const span = document.createElement("span");
+            const editImg = document.createElement("img");
+            let editBtn = document.createElement("button");
+            editImg.src = "/images/edit-pen.png"
+
             li.textContent = inputBox.value;
-            listContainer.appendChild(li);
-            let span = document.createElement("span");
+            li.appendChild(editBtn);
+            editBtn.appendChild(editImg);
             span.textContent = "\u00d7";
             li.appendChild(span);
+
+            listContainer.appendChild(li);
             saveData();
-            // hintMsg();
         }
         inputBox.value = "";
         saveData();
@@ -79,6 +84,15 @@ window.addEventListener('load', () => {
             e.target.parentElement.remove();
             saveData();
         }
+        else if (e.target.tagName === "IMG" || e.target.tagName === "BUTTON"){
+            inputBox.focus();
+            inputBox.addEventListener('blur', e => {
+                saveData();
+                showTask()
+            })
+            // e.target.parentElement.focus();
+            saveData();
+        }
     }, false)
 
     // Saving the data to the browser's local storage so that reloads, refreshing or closing of the page doesn't affect it.
@@ -91,3 +105,15 @@ window.addEventListener('load', () => {
         listContainer.innerHTML = localStorage.getItem("savedata");
     }
     showTask();
+
+    let filterInput = document.querySelector("#filter-btn");
+    filterCount = 1;
+
+    function createFilterInput() {
+        for (i = 0; i < filterCount; i--) {
+            const filterTextBox = document.createElement("input");
+            const input = document.createElement("input");
+            input.setAttribute("type", "text");
+            document.body.appendChild(input);
+        }
+    }
